f6bb15d722c430696aaccbe8f043360521b40b02
CayaN <30707106+cayanxk@users.noreply.github.com>
2021-03-15 22:16:36 +0300

    Create checkAccount.js

---
+++ utils/checkAccount.js
@@ -0,0 +1,26 @@
+const User = require("../models/User");
+const twofactor = require("node-2fa");
+
+const checkAccount = async (email, password, secretKey) => {
+  const userAccount = await User.findOne({ email: email });
+  if (!userAccount) return { code: 404, message: "Email is not found" };
+
+  if (userAccount.password != password) {
+    return { code: 401, message: "The password is wrong" };
+  }
+
+  if (!secretKey)
+    return { code: 400, message: "Missing field required", field: "secretKey" };
+  const getKey = twofactor.generateToken(userAccount.secretKey);
+  console.log(JSON.stringify(getKey));
+
+  if (getKey.token != secretKey) {
+    return { code: 401, message: "The secret key is wrong" };
+  } else {
+    return { code: 200, message: `Welcome, ${userAccount.username}!` };
+  }
+};
+
+module.exports = {
+  checkAccount,
+};
