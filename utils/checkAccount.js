8cd6d78d4c3485db44c2b8b70f631d34a0157854
CayaN <30707106+cayanxk@users.noreply.github.com>
2021-03-15 22:50:29 +0300

    Update checkAccount.js

--- utils/checkAccount.js
+++ utils/checkAccount.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 const User = require("../src/models/User");
 const twofactor = require("node-2fa");
 
@@ -11,8 +12,11 @@ const checkAccount = async (email, password, secretKey) => {
 
   if (!secretKey)
     return { code: 400, message: "Missing field required", field: "secretKey" };
+
   const getKey = twofactor.generateToken(userAccount.secretKey);
-  console.log(JSON.stringify(getKey));
+  if (process.env.NODE_ENV === "development") {
+    console.log(JSON.stringify(getKey));
+  }
 
   if (getKey.token != secretKey) {
     return { code: 401, message: "The secret key is wrong" };
