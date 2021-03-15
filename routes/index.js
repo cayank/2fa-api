84467b8d79cbc763d180eaadf523d1a3a8e37e51
CayaN <30707106+cayanxk@users.noreply.github.com>
2021-03-15 22:16:55 +0300

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var fs = require("fs");
+var routerFiles = fs.readdirSync(__dirname + "/api");
+/**
+ * @param app Express app
+ */
+
+const init = (app) => {
+  routerFiles.forEach((file) => {
+    app.use("/api", require(__dirname + "\\api\\" + file));
+  });
+
+  app.all("*", (req, res) => {
+    return res
+      .status(404)
+      .json({
+        code: 404,
+        message: "Page not found",
+      })
+      .end();
+  });
+};
+
+module.exports = { init };
